925d7dc5f83b5c3658ce0d1b66b6e829a43769fc
Charles Vardeman <charles.vardeman@gmail.com>
2022-06-02 10:48:43 -0400

    Adding Script for Bashlib alias

---
+++ setup-bashlib.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# From: https://github.com/SolidLabResearch/Bashlib/blob/master/Tutorial-cli.md
+
+alias bashlib-auth="node Bashlib/bashlib/solid/bin/solid.js --auth token -t ~/.solid/.css-auth-token"
+alias bashlib-css="node Bashlib/bashlib/css/bin/css.js"
+alias bashlib-solid="node Bashlib/bashlib/solid/bin/solid.js"
